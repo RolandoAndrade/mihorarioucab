7adcfcde7694a218cc79f7b620b1ffa7a4f29eaa
Super Proyectos <rolandoandrade.estudio@gamil.com>
2018-12-02 21:22:47 -0400

    Añadido staticsfiles para deploy

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,5 @@ USE_TZ = True
 
 STATIC_URL = '/resources/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'resources'),)
+
+django_heroku.settings(locals())
